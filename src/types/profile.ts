d2a674d12a17ddb1de6277b3e3c842e573c00d68
rolf-rayk <ray.kong@theriverflows.org>
2026-05-27 16:07:34 -0700

    created child profile interface

---
+++ src/types/profile.ts
@@ -0,0 +1,12 @@
+export interface ChildProfile {
+    id: string;
+    firstName: string;
+    lastName: string;
+    birthYear: number;
+    birthMonth: number;
+    birthDay: number;
+    age: number;
+    country: string;
+    createdAt: Date;
+    profilePictureURL: string;
+}
